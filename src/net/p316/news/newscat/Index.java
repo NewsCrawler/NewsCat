8ce985284d13dba3f528db467c86a9eb83bc10d5
GyupaRupa <tuvisa96@gmail.com>
2016-11-07 16:47:46 +0900

    에베베베베

--- src/net/p316/news/newscat/Index.java
+++ src/net/p316/news/newscat/Index.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/"})
+@WebServlet(urlPatterns = { "/Index"})
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public static final String HTML_START="<html><body>";
